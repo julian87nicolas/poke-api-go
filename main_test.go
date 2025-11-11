66f149f2eafbc06109e6884933d53972afb80cdf
Julian Camargo <julicmrgo@gmail.com>
2025-11-11 20:37:09 -0300

    Primer test

---
+++ main_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestAddSuccess(t *testing.T) {
+	result := Add(20, 2)
+
+	expect := 22
+
+	if result != expect {
+		t.Errorf("got %d, excpected %d", result, expect)
+	}
+}
